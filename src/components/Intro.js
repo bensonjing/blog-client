691093dcff51235480af263076f1fdeb6a15e58e
Benson Jing <bensonjing23@gmail.com>
2022-08-14 20:20:43 -0400

    css: add intro style

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,10 +1,12 @@
 function Intro() {
   return (
-    <div id="intro" className="container">
-      <div>Welcome to My Blog</div>
-      <div>
-        Read and share ideas from independent voices, world-class publications,
-        and experts from around the globe. Everyone's welcome.
+    <div id="intro">
+      <div id="left">
+        <h1 className="title">Welcome to My Blog</h1>
+        <p>
+          Read and share ideas from independent voices, world-class
+          publications, and experts from around the globe. Everyone's welcome.
+        </p>
       </div>
     </div>
   );
